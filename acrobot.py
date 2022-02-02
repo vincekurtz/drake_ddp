5622fcbb3dd8d9cfe497a641b3a0dd372f40dc3f
Vince Kurtz <vjkurtz@gmail.com>
2022-02-02 11:39:38 -0500

    acrobot swingup with direct transcription

--- acrobot.py
+++ acrobot.py
@@ -14,8 +14,8 @@ import time
 # Parameters
 ####################################
 
-T = 10         # total simulation time (S)
-dt = 1e-3      # simulation timestep
+T = 2          # total simulation time (S)
+dt = 1e-2      # simulation timestep
 
 # CONST = constant (zero) torque
 # DT = direct transcription
@@ -23,7 +23,7 @@ dt = 1e-3      # simulation timestep
 control_method = "CONST" 
 
 # Initial state
-x0 = np.array([0,np.pi/2,0,0])
+x0 = np.array([0,0,0,0])
 
 ####################################
 # Create system diagram
@@ -58,31 +58,72 @@ ConnectContactResultsToDrakeVisualizer(builder, plant, scene_graph)
 diagram = builder.Build()
 diagram_context = diagram.CreateDefaultContext()
 
-####################################
-# Run simulation
-####################################
+######################################
+## Set up simulation
+######################################
 simulator = Simulator(diagram, diagram_context)
 simulator.set_target_realtime_rate(-1.0)
 
 plant_context = diagram.GetMutableSubsystemContext(
-        plant, simulator.get_mutable_context())
+        plant, diagram_context)
 plant.SetPositionsAndVelocities(plant_context, x0)
 
-simulator.AdvanceTo(T)
+#simulator.AdvanceTo(T)
+
+#####################################
+# Solve Trajectory Optimization
+#####################################
+
+# Create a system model to do the optimization over
+plant_ = MultibodyPlant(dt)
+Parser(plant_).AddModelFromFile(urdf)
+plant_.Finalize()
+context_ = plant_.CreateDefaultContext()
+
+# Set up the solver object
+trajopt = DirectTranscription(
+        plant_, context_, 
+        input_port_index=plant.get_actuation_input_port().get_index(),
+        num_time_samples=int(T/dt))
+
+# Add constraints
+x = trajopt.state()
+u = trajopt.input()
+x_init = trajopt.initial_state()
+
+trajopt.prog().AddConstraint(eq( x_init, x0 ))
+
+x_err = x - np.array([np.pi, 0, 0, 0])
+
+trajopt.AddRunningCost(0.01*x_err.T@x_err + 0.01*u.T@u)
+trajopt.AddFinalCost(200*x_err.T@x_err)
+
+# Solve the optimization problem
+st = time.time()
+res = Solve(trajopt.prog())
+solve_time = time.time() - st
+assert res.is_success(), "trajectory optimizer failed"
+solver_name = res.get_solver_id().name()
+print(f"Solved in {solve_time} seconds using {solver_name}")
+
+# Extract the solution
+timesteps = trajopt.GetSampleTimes(res)
+states = trajopt.GetStateSamples(res)
+inputs = trajopt.GetInputSamples(res)
 
 #####################################
 # Playback on visualizer
 #####################################
-log = logger.FindLog(diagram_context)
-states = log.data()
-timesteps = log.sample_times()
 
-for i in range(len(timesteps)):
-    t = timesteps[i]
-    x = states[:,i]
+while True:
+    # Just keep playing back the trajectory
+    for i in range(len(timesteps)):
+        t = timesteps[i]
+        x = states[:,i]
 
-    diagram_context.SetTime(t)
-    plant.SetPositionsAndVelocities(plant_context, x)
-    diagram.Publish(diagram_context)
+        diagram_context.SetTime(t)
+        plant.SetPositionsAndVelocities(plant_context, x)
+        diagram.Publish(diagram_context)
 
-    time.sleep(dt-3e-4)
+        time.sleep(dt-3e-4)
+    time.sleep(1)
