f01afcc846f40d959c7f60a0c61bdfd687c8692d
Vince Kurtz <vjkurtz@gmail.com>
2022-02-08 15:53:55 -0500

    misc cleanup

--- cart_pole_with_wall.py
+++ cart_pole_with_wall.py
@@ -9,19 +9,27 @@
 
 from pydrake.all import *
 
-# Some simulation parameters 
-T = 2.0
+####################################
+# Parameters
+####################################
+
+T = 2.0 
 dt = 1e-3
 
-radius = 0.05
-mass = 0.1
+# Initial state
+x0 = np.array([0,np.pi-0.2,0,0])
 
 # Contact model parameters
-dissipation = 5.0              # controls "bounciness" of collisions: lower is bouncier
-hydroelastic_modulus = 5e4     # controls "squishiness" of collisions: lower is squishier
+dissipation = 0.0              # controls "bounciness" of collisions: lower is bouncier
+hydroelastic_modulus = 2e5     # controls "squishiness" of collisions: lower is squishier
 resolution_hint = 0.05         # smaller means a finer mesh
 
-# Set up system diagram
+contact_model = ContactModel.kPoint  # Hydroelastic, Point, or HydroelasticWithFallback
+mesh_type = HydroelasticContactRepresentation.kPolygon  # Triangle or Polygon
+
+####################################
+# Create system diagram
+####################################
 builder = DiagramBuilder()
 plant, scene_graph = AddMultibodyPlantSceneGraph(builder, dt)
 
@@ -30,18 +38,18 @@ sdf = FindResourceOrThrow("drake/examples/multibody/cart_pole/cart_pole.sdf")
 robot = Parser(plant=plant).AddModelFromFile(sdf)
 
 # Add a ball with compliant hydroelastic contact to the end of the cart-pole system
+radius = 0.05
 pole = plant.GetBodyByName("Pole")
 X_BP = RigidTransform()
 ball_props = ProximityProperties()
 AddCompliantHydroelasticProperties(resolution_hint, hydroelastic_modulus, ball_props)
 AddContactMaterial(dissipation=dissipation, friction=CoulombFriction(), properties=ball_props)
 plant.RegisterCollisionGeometry(pole, X_BP, Sphere(radius), "collision", ball_props)
-
 orange = np.array([1.0, 0.55, 0.0, 0.5])
 plant.RegisterVisualGeometry(pole, X_BP, Sphere(radius), "visual", orange)
 
 # Add a wall with rigid hydroelastic contact
-l,w,h = (0.05,1,2)   
+l,w,h = (0.1,1,2)   
 I_W = SpatialInertia(1, np.zeros(3), UnitInertia.SolidBox(l,w,h))
 wall_instance = plant.AddModelInstance("wall")
 wall = plant.AddRigidBody("wall", wall_instance, I_W)
@@ -59,9 +67,8 @@ plant.RegisterCollisionGeometry(wall, RigidTransform(),
         Box(l,w,h), "wall_collision", wall_props)
 
 # Choose contact model
-plant.set_contact_surface_representation(
-        HydroelasticContactRepresentation.kPolygon)
-plant.set_contact_model(ContactModel.kHydroelastic)
+plant.set_contact_surface_representation(mesh_type)
+plant.set_contact_model(contact_model)
 plant.Finalize()
 
 # Connect to visualizer
@@ -72,6 +79,10 @@ ConnectContactResultsToDrakeVisualizer(builder, plant, scene_graph)
 diagram = builder.Build()
 diagram_context = diagram.CreateDefaultContext()
 plant_context = diagram.GetMutableSubsystemContext(plant, diagram_context)
+
+###################################
+# Run Simulation
+###################################
 
 # Fix zero input for now
 plant.get_actuation_input_port().FixValue(plant_context, 0)
