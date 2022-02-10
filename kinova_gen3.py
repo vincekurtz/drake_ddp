eda851a31428b51f9dd1e8eb78d87f92ed861a33
Vince Kurtz <vjkurtz@gmail.com>
2022-02-10 11:45:50 -0500

    kicking ball example

--- kinova_gen3.py
+++ kinova_gen3.py
@@ -15,26 +15,35 @@ from ilqr import IterativeLinearQuadraticRegulator
 # Parameters
 ####################################
 
-T = 2.0
-dt = 5e-3
-playback_rate = 0.2
+T = 0.5
+dt = 1e-2
+playback_rate = 1.0
 
 # Some useful joint angle definitions
 q_home = np.array([0, np.pi/12, np.pi, 4.014-2*np.pi, 0, 0.9599, np.pi/2])
 q_retract = np.array([0, 5.93-2*np.pi, np.pi, 3.734-2*np.pi, 0, 5.408-2*np.pi, np.pi/2])
+q_start = np.array([0.25, np.pi/3, np.pi, 4.4-2*np.pi, 0, 0.9599, np.pi/2])
 
-q_ball = np.array([0,0,0,1,0.3,0,0.1])
+q_ball_start = np.array([0,0,0,1,0.5,0,0.1])
+q_ball_target = np.array([0,0,0,1,0.5,0.1,0.1])
 
 # Initial state
-x0 = np.hstack([q_home, q_ball, np.zeros(13)])
+x0 = np.hstack([q_start, q_ball_start, np.zeros(13)])
 
 # Target state
-x_nom = np.hstack([q_home, q_ball, np.zeros(13)])
+x_nom = np.hstack([q_start, q_ball_target, np.zeros(13)])
 
 # Quadratic cost
-Q = np.diag(np.hstack([2*np.ones(7),5*np.ones(7)]))
+Qq_robot = 0.0*np.ones(7)
+Qv_robot = 1.0*np.ones(7)
+Qq_ball = np.array([0,0,0,0,100,100,100])
+Qv_ball = np.ones(6)
+Q_diag = np.hstack([Qq_robot, Qq_ball, Qv_robot, Qv_ball])
+Qf_diag = np.hstack([Qq_robot, 10*Qq_ball, Qv_robot, 10*Qv_ball])
+
+Q = np.diag(Q_diag)
 R = 0.01*np.eye(7)
-Qf = np.diag(np.hstack([5*np.ones(7),1*np.ones(7)]))
+Qf = np.diag(Qf_diag)
 
 # Contact model parameters
 dissipation = 1.0              # controls "bounciness" of collisions: lower is bouncier
@@ -63,7 +72,8 @@ def create_system_model(plant):
     # Add a ground with compliant hydroelastic contact
     ground_props = ProximityProperties()
     AddCompliantHydroelasticPropertiesForHalfSpace(1.0,hydroelastic_modulus,ground_props)
-    AddContactMaterial(dissipation=dissipation, friction=CoulombFriction(), properties=ground_props)
+    #AddContactMaterial(dissipation=dissipation, friction=CoulombFriction(), properties=ground_props)
+    AddContactMaterial(friction=CoulombFriction(), properties=ground_props)
     plant.RegisterCollisionGeometry(plant.world_body(), RigidTransform(),
             HalfSpace(), "ground_collision", ground_props)
 
@@ -77,7 +87,8 @@ def create_system_model(plant):
 
     ball_props = ProximityProperties()
     AddCompliantHydroelasticProperties(resolution_hint, hydroelastic_modulus,ball_props)
-    AddContactMaterial(dissipation=dissipation, friction=CoulombFriction(), properties=ball_props)
+    #AddContactMaterial(dissipation=dissipation, friction=CoulombFriction(), properties=ball_props)
+    AddContactMaterial(friction=CoulombFriction(), properties=ball_props)
     plant.RegisterCollisionGeometry(ball, X_ball, Sphere(radius),
             "ball_collision", ball_props)
 
@@ -107,73 +118,75 @@ diagram = builder.Build()
 diagram_context = diagram.CreateDefaultContext()
 plant_context = diagram.GetMutableSubsystemContext(plant, diagram_context)
 
-######################################
-### Solve Trajectory Optimization
-######################################
-#
-## Create a system model (w/o visualizer) to do the optimization over
-#builder_ = DiagramBuilder()
-#plant_, scene_graph_ = AddMultibodyPlantSceneGraph(builder_, dt)
-#plant_ = create_system_model(plant_)
-#builder_.ExportInput(plant_.get_actuation_input_port(), "control")
-#system_ = builder_.Build()
-#
-## Set up the optimizer
-#num_steps = int(T/dt)
-#ilqr = IterativeLinearQuadraticRegulator(system_, num_steps, 
-#        beta=0.9, delta=1e-2, gamma=0)
-#
-## Define the optimization problem
-#ilqr.SetInitialState(x0)
-#ilqr.SetTargetState(x_nom)
-#ilqr.SetRunningCost(dt*Q, dt*R)
-#ilqr.SetTerminalCost(Qf)
-#
-## Set initial guess (gravity compensation)
-#plant.SetPositionsAndVelocities(plant_context, x0)
-#tau_g = -plant.CalcGravityGeneralizedForces(plant_context)
-#
-#u_guess = np.repeat(tau_g[np.newaxis].T, num_steps-1, axis=1)
-#ilqr.SetInitialGuess(u_guess)
-#
-## Solve the optimization problem
-#states, inputs, solve_time, optimal_cost = ilqr.Solve()
-#print(f"Solved in {solve_time} seconds using iLQR")
-#print(f"Optimal cost: {optimal_cost}")
-#timesteps = np.arange(0.0,T,dt)
-#
-#######################################
-### Playback
-#######################################
-#
-#while True:
-#    plant.get_actuation_input_port().FixValue(plant_context, 
-#            np.zeros(plant.num_actuators()))
-#    # Just keep playing back the trajectory
-#    for i in range(len(timesteps)):
-#        t = timesteps[i]
-#        x = states[:,i]
-#
-#        diagram_context.SetTime(t)
-#        plant.SetPositionsAndVelocities(plant_context, x)
-#        diagram.Publish(diagram_context)
-#
-#        time.sleep(1/playback_rate*dt-4e-4)
+####################################
+# Solve Trajectory Optimization
+####################################
+
+# Create a system model (w/o visualizer) to do the optimization over
+builder_ = DiagramBuilder()
+plant_, scene_graph_ = AddMultibodyPlantSceneGraph(builder_, dt)
+plant_ = create_system_model(plant_)
+builder_.ExportInput(plant_.get_actuation_input_port(), "control")
+system_ = builder_.Build()
+
+# Set up the optimizer
+num_steps = int(T/dt)
+ilqr = IterativeLinearQuadraticRegulator(system_, num_steps, 
+        beta=0.9, delta=1e-2, gamma=0)
+
+# Define the optimization problem
+ilqr.SetInitialState(x0)
+ilqr.SetTargetState(x_nom)
+ilqr.SetRunningCost(dt*Q, dt*R)
+ilqr.SetTerminalCost(Qf)
+
+# Set initial guess (gravity compensation)
+plant.SetPositionsAndVelocities(plant_context, x0)
+tau_g = -plant.CalcGravityGeneralizedForces(plant_context)
+S = plant.MakeActuationMatrix().T
+u_guess = S@np.repeat(tau_g[np.newaxis].T, num_steps-1, axis=1)
+
+#u_guess = np.zeros((plant.num_actuators(),num_steps-1))
+ilqr.SetInitialGuess(u_guess)
+
+# Solve the optimization problem
+states, inputs, solve_time, optimal_cost = ilqr.Solve()
+print(f"Solved in {solve_time} seconds using iLQR")
+print(f"Optimal cost: {optimal_cost}")
+timesteps = np.arange(0.0,T,dt)
+
+#####################################
+# Playback
+#####################################
+
+while True:
+    plant.get_actuation_input_port().FixValue(plant_context, 
+            np.zeros(plant.num_actuators()))
+    # Just keep playing back the trajectory
+    for i in range(len(timesteps)):
+        t = timesteps[i]
+        x = states[:,i]
+
+        diagram_context.SetTime(t)
+        plant.SetPositionsAndVelocities(plant_context, x)
+        diagram.Publish(diagram_context)
+
+        time.sleep(1/playback_rate*dt-4e-4)
 #    time.sleep(1)
 
 #####################################
 ## Run Simulation
 #####################################
-
-# Fix zero input for now
-plant.get_actuation_input_port().FixValue(plant_context, np.zeros(plant.num_actuators()))
-
-# Set initial state
-plant.SetPositionsAndVelocities(plant_context, x0)
-
-# Simulate the system
-simulator = Simulator(diagram, diagram_context)
-simulator.set_target_realtime_rate(playback_rate)
-simulator.set_publish_every_time_step(True)
-
-simulator.AdvanceTo(T)
+#
+## Fix zero input for now
+#plant.get_actuation_input_port().FixValue(plant_context, np.zeros(plant.num_actuators()))
+#
+## Set initial state
+#plant.SetPositionsAndVelocities(plant_context, x0)
+#
+## Simulate the system
+#simulator = Simulator(diagram, diagram_context)
+#simulator.set_target_realtime_rate(playback_rate)
+#simulator.set_publish_every_time_step(True)
+#
+#simulator.AdvanceTo(T)
