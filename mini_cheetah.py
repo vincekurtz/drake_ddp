61408f88964de982acc805832fd167f196d189bd
Vince Kurtz <vjkurtz@gmail.com>
2022-02-10 15:38:03 -0500

    mini cheetah shifting base example

--- mini_cheetah.py
+++ mini_cheetah.py
@@ -15,7 +15,7 @@ from ilqr import IterativeLinearQuadraticRegulator
 # Parameters
 ####################################
 
-T = 0.2
+T = 0.5
 dt = 1e-2
 playback_rate = 0.2
 
@@ -26,31 +26,36 @@ q0 = np.asarray([ 1.0, 0.0, 0.0, 0.0,     # base orientation
                   0.0,-0.8, 1.6,
                   0.0,-0.8, 1.6,
                   0.0,-0.8, 1.6])
-u_stand = np.array([-0.68308268,  0.23838864, -2.7471892 ,  0.90897645,  0.17443321,
-       -2.13045122, -0.55760413,  0.21117006, -2.48200535,  0.78522387,
-        0.1466108 , -1.86508232])
+u_stand = np.array([ 0.16370625,  0.42056475, -3.06492254,  0.16861717,  0.14882384,
+       -2.43250739,  0.08305763,  0.26016952, -2.74586461,  0.08721941,
+        0.02331732, -2.18319231])
 
 # Initial state
 x0 = np.hstack([q0, np.zeros(18)])
 
 # Target state
 x_nom = np.hstack([q0, np.zeros(18)])
-x_nom[6] += 0.10  # base z position
+x_nom[4] += 0.10  # base x position
+x_nom[5] += 0.10  # base y position
+x_nom[6] += 0.00  # base z position
 
 # Quadratic cost
-Qq_base = 10*np.ones(7)
+Qq_base = np.ones(7)
 Qv_base = np.ones(6)
 
-Qq_legs = np.ones(12)
-Qv_legs = np.ones(12)
+Qq_legs = 0.0*np.ones(12)
+Qv_legs = 0.01*np.ones(12)
 
-Q = np.diag(np.hstack([Qq_base,Qq_legs,Qv_base,Qv_legs]))
+Q = np.diag(np.hstack([Qq_base,Qq_legs,0.01*Qv_base,Qv_legs]))
 R = 0.01*np.eye(12)
-Qf = Q
+Qf = np.diag(np.hstack([5*Qq_base,Qq_legs,Qv_base,Qv_legs]))
 
 # Contact model parameters
 contact_model = ContactModel.kHydroelastic  # Hydroelastic, Point, or HydroelasticWithFallback
 mesh_type = HydroelasticContactRepresentation.kPolygon  # Triangle or Polygon
+
+mu_static = 0.5
+mu_dynamic = 0.5
 
 ####################################
 # Tools for system setup
@@ -65,7 +70,8 @@ def create_system_model(plant):
     # Add a ground with rigid hydroelastic contact
     ground_props = ProximityProperties()
     AddRigidHydroelasticProperties(ground_props)
-    AddContactMaterial(friction=CoulombFriction(), properties=ground_props)
+    friction = CoulombFriction(mu_static, mu_dynamic)
+    AddContactMaterial(friction=friction, properties=ground_props)
     plant.RegisterCollisionGeometry(plant.world_body(), RigidTransform(),
             HalfSpace(), "ground_collision", ground_props)
 
@@ -92,12 +98,11 @@ diagram = builder.Build()
 diagram_context = diagram.CreateDefaultContext()
 plant_context = diagram.GetMutableSubsystemContext(plant, diagram_context)
 
-####################################
-# Solve Trajectory Optimization
-####################################
-
-# Create a system model (w/o visualizer) to do the optimization over
-builder_ = DiagramBuilder()
+##################################### 
+# Solve Trajectory Optimization 
+#################################### 
+# Create a system model (w/o visualizer) to do the optimization over 
+builder_ = DiagramBuilder() 
 plant_, scene_graph_ = AddMultibodyPlantSceneGraph(builder_, dt)
 plant_ = create_system_model(plant_)
 builder_.ExportInput(plant_.get_actuation_input_port(), "control")
@@ -115,8 +120,8 @@ ilqr.SetRunningCost(dt*Q, dt*R)
 ilqr.SetTerminalCost(Qf)
 
 # Set initial guess
-u_guess = np.zeros((plant.num_actuators(),num_steps-1))
-#u_guess = np.repeat(u_stand[np.newaxis].T,num_steps-1,axis=1)
+#u_guess = np.zeros((plant.num_actuators(),num_steps-1))
+u_guess = np.repeat(u_stand[np.newaxis].T,num_steps-1,axis=1)
 ilqr.SetInitialGuess(u_guess)
 
 # Solve the optimization problem
@@ -150,7 +155,8 @@ while True:
 #####################################
 #
 ## Fix zero input for now
-#plant.get_actuation_input_port().FixValue(plant_context, np.zeros(plant.num_actuators()))
+##plant.get_actuation_input_port().FixValue(plant_context, np.zeros(plant.num_actuators()))
+#plant.get_actuation_input_port().FixValue(plant_context, u_stand)
 #
 ## Set initial state
 #plant.SetPositionsAndVelocities(plant_context, x0)
