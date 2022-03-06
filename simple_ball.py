0ad79a8fa3c88aa9da66e6e6085604d0ee02914d
Vince Kurtz <vjkurtz@gmail.com>
2022-03-05 20:01:38 -0500

    print forward dynamics partials and auodiff times

--- simple_ball.py
+++ simple_ball.py
@@ -8,6 +8,7 @@
 ##
 
 from pydrake.all import *
+import time
 
 # Some simulation parameters 
 T = 2.0
@@ -27,61 +28,65 @@ surface_type = HydroelasticContactRepresentation.kPolygon  # Polygon or Triangle
 builder = DiagramBuilder()
 plant, scene_graph = AddMultibodyPlantSceneGraph(builder, dt)
 
-# Add a ball with compliant hydroelastic contact
-G_Bcm = UnitInertia.SolidSphere(radius)
-M_Bcm = SpatialInertia(mass, np.zeros(3), G_Bcm)
-ball = plant.AddRigidBody("ball", M_Bcm)
+def create_system_model(plant, scene_graph):
+    # Add a ball with compliant hydroelastic contact
+    G_Bcm = UnitInertia.SolidSphere(radius)
+    M_Bcm = SpatialInertia(mass, np.zeros(3), G_Bcm)
+    ball = plant.AddRigidBody("ball", M_Bcm)
 
-X_BS = RigidTransform()
-ball_props = ProximityProperties()
-AddCompliantHydroelasticProperties(resolution_hint, hydroelastic_modulus, ball_props)
-AddContactMaterial(dissipation=dissipation, friction=CoulombFriction(mu,mu), properties=ball_props)
-plant.RegisterCollisionGeometry(ball, X_BS, Sphere(radius),
-        "ball_collision", ball_props)
+    X_BS = RigidTransform()
+    ball_props = ProximityProperties()
+    AddCompliantHydroelasticProperties(resolution_hint, hydroelastic_modulus, ball_props)
+    AddContactMaterial(dissipation=dissipation, friction=CoulombFriction(mu,mu), properties=ball_props)
+    plant.RegisterCollisionGeometry(ball, X_BS, Sphere(radius),
+            "ball_collision", ball_props)
 
-orange = np.array([1.0, 0.55, 0.0, 0.5])
-plant.RegisterVisualGeometry(ball, X_BS, Sphere(radius), "visual", orange)
-    
-# Add some spots to visualize the ball's roation
-spot_color = np.array([0.0,0.00,0.0,0.5])
-spot_radius = 0.1*radius
-spot = Sphere(spot_radius)
-spot_offset = radius - 0.9*spot_radius;
+    orange = np.array([1.0, 0.55, 0.0, 0.5])
+    plant.RegisterVisualGeometry(ball, X_BS, Sphere(radius), "visual", orange)
+        
+    # Add some spots to visualize the ball's roation
+    spot_color = np.array([0.0,0.00,0.0,0.5])
+    spot_radius = 0.1*radius
+    spot = Sphere(spot_radius)
+    spot_offset = radius - 0.9*spot_radius;
 
-plant.RegisterVisualGeometry(
-        ball, RigidTransform(RotationMatrix(),np.array([spot_offset,0,0])),
-        spot, "sphere_x+", spot_color)
-plant.RegisterVisualGeometry(
-        ball, RigidTransform(RotationMatrix(),np.array([-spot_offset,0,0])),
-        spot, "sphere_x-", spot_color)
-plant.RegisterVisualGeometry(
-        ball, RigidTransform(RotationMatrix(),np.array([0,spot_offset,0])),
-        spot, "sphere_y+", spot_color)
-plant.RegisterVisualGeometry(
-        ball, RigidTransform(RotationMatrix(),np.array([0,-spot_offset,0])),
-        spot, "sphere_y-", spot_color)
-plant.RegisterVisualGeometry(
-        ball, RigidTransform(RotationMatrix(),np.array([0,0,spot_offset])),
-        spot, "sphere_z+", spot_color)
-plant.RegisterVisualGeometry(
-        ball, RigidTransform(RotationMatrix(),np.array([0,0,-spot_offset])),
-        spot, "sphere_z-", spot_color)
+    plant.RegisterVisualGeometry(
+            ball, RigidTransform(RotationMatrix(),np.array([spot_offset,0,0])),
+            spot, "sphere_x+", spot_color)
+    plant.RegisterVisualGeometry(
+            ball, RigidTransform(RotationMatrix(),np.array([-spot_offset,0,0])),
+            spot, "sphere_x-", spot_color)
+    plant.RegisterVisualGeometry(
+            ball, RigidTransform(RotationMatrix(),np.array([0,spot_offset,0])),
+            spot, "sphere_y+", spot_color)
+    plant.RegisterVisualGeometry(
+            ball, RigidTransform(RotationMatrix(),np.array([0,-spot_offset,0])),
+            spot, "sphere_y-", spot_color)
+    plant.RegisterVisualGeometry(
+            ball, RigidTransform(RotationMatrix(),np.array([0,0,spot_offset])),
+            spot, "sphere_z+", spot_color)
+    plant.RegisterVisualGeometry(
+            ball, RigidTransform(RotationMatrix(),np.array([0,0,-spot_offset])),
+            spot, "sphere_z-", spot_color)
 
-# Add a ground with rigid hydroelastic contact
-ground_props = ProximityProperties()
-AddRigidHydroelasticProperties(ground_props)
-AddContactMaterial(dissipation=dissipation, friction=CoulombFriction(mu, mu), properties=ground_props)
-plant.RegisterCollisionGeometry(
-        plant.world_body(), RigidTransform(), HalfSpace(), 
-        "ground_collision", ground_props)
+    # Add a ground with rigid hydroelastic contact
+    ground_props = ProximityProperties()
+    AddRigidHydroelasticProperties(ground_props)
+    AddContactMaterial(dissipation=dissipation, friction=CoulombFriction(mu, mu), properties=ground_props)
+    plant.RegisterCollisionGeometry(
+            plant.world_body(), RigidTransform(), HalfSpace(), 
+            "ground_collision", ground_props)
 
-# Turn off gravity
-#plant.mutable_gravity_field().set_gravity_vector([0,0,0])
+    # Turn off gravity
+    #plant.mutable_gravity_field().set_gravity_vector([0,0,0])
 
-# Choose contact model
-plant.set_contact_surface_representation(surface_type)
-plant.set_contact_model(ContactModel.kHydroelastic)
-plant.Finalize()
+    # Choose contact model
+    plant.set_contact_surface_representation(surface_type)
+    plant.set_contact_model(ContactModel.kHydroelastic)
+    plant.Finalize()
+
+# Add the system model
+create_system_model(plant, scene_graph)
 
 # Connect to visualizer
 DrakeVisualizer().AddToBuilder(builder, scene_graph)
@@ -92,13 +97,47 @@ diagram = builder.Build()
 diagram_context = diagram.CreateDefaultContext()
 plant_context = diagram.GetMutableSubsystemContext(plant, diagram_context)
 
-# Simulate the system
-q0 = np.array([0,0,0,1,-0.5,0,0.05])
+# Set initial conditions
+q0 = np.array([0,0,0,1,-0.5,0,0.047])
 v0 = np.array([0,0,0,0.5,0,0.0])
 plant.SetPositions(plant_context, q0)
 plant.SetVelocities(plant_context, v0)
 
-simulator = Simulator(diagram, diagram_context)
-simulator.set_target_realtime_rate(realtime_rate)
+# Create a separate system model for autodiff.
+# This is essentially what the iLQR controller will have access to.
+builder_ = DiagramBuilder()
+plant_, scene_graph_ = AddMultibodyPlantSceneGraph(builder_, dt)
+create_system_model(plant_, scene_graph_)
+system_ = builder_.Build()
+context_ = system_.CreateDefaultContext()
 
-simulator.AdvanceTo(T)
+# Compute dynamics x_{t+1} = f(x) explicitly
+st = time.time()
+x = np.hstack([q0, v0])
+context_.SetDiscreteState(x)
+state = context_.get_discrete_state()
+st = time.time()
+system_.CalcDiscreteVariableUpdates(context_, state)
+et = time.time()-st
+x_next = state.get_vector().value().flatten()
+print("Computed forward dynamics in ",et)
+#print("x_next: ", x_next)
+
+# Compute dynamics partials f_x via autodiff
+system_ad = system_.ToAutoDiffXd()
+context_ad = system_ad.CreateDefaultContext()
+x_ad = InitializeAutoDiff(x)
+context_ad.SetDiscreteState(x_ad)
+state = context_ad.get_discrete_state()
+st = time.time()
+system_ad.CalcDiscreteVariableUpdates(context_ad, state)
+et = time.time()-st
+x_next_ad = state.get_vector().CopyToVector()
+fx = ExtractGradient(x_next_ad)
+print("Computed forward dynamics partials in ",et)
+#print("f_x: ",fx)
+
+## Simulate the sytem
+#simulator = Simulator(diagram, diagram_context)
+#simulator.set_target_realtime_rate(realtime_rate)
+#simulator.AdvanceTo(T)
