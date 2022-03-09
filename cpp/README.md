10f41c679c6552ec483115574e43ef84aac0b05c
Vince Kurtz <vjkurtz@gmail.com>
2022-03-08 20:44:59 -0500

    cpp hello world

---
+++ cpp/README.md
@@ -0,0 +1,9 @@
+## Build Instructions
+
+```
+mkdir -p build && cd build
+cmake ..
+make -j4
+```
+
+Compiled binaries will be in the build directory.
---
+++ cpp/src/simple_ball.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/**
+ *
+ * Run some tests for a simple ball
+ * with hydroelastic contact.
+ *
+ */
+
+int main() {
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+}
