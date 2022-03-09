32efd2a10187d1cb6d162e948394d7405b378458
Vince Kurtz <vjkurtz@gmail.com>
2022-03-08 21:32:14 -0500

    start building plant model

--- cpp/src/simple_ball.cpp
+++ cpp/src/simple_ball.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+#include <drake/geometry/scene_graph.h>
+#include <drake/multibody/parsing/parser.h>
+#include <drake/multibody/plant/multibody_plant.h>
+#include "drake/multibody/plant/multibody_plant_config_functions.h"
+#include <drake/systems/framework/context.h>
+#include <drake/systems/framework/diagram_builder.h>
+
 /**
  *
  * Run some tests for a simple ball
@@ -7,7 +14,37 @@
  *
  */
 
+using namespace drake;
+
+using systems::Context;
+using systems::Diagram;
+using systems::DiagramBuilder;
+using multibody::MultibodyPlant;
+using multibody::MultibodyPlantConfig;
+using geometry::SceneGraph;
+
 int main() {
+
+  // Plant parameters
+  MultibodyPlantConfig config;
+  config.time_step = 1e-2;
+  config.contact_model = "hydroelastic";
+  config.contact_surface_representation = "polygon";
+
+  // Ball parameters
+  double radius = 0.05;
+  double mass = 0.1;
+
+  // Contact parameters
+  double dissipation = 0.0;
+  double hydroelastic_modulus = 5e4;
+  double resolution_hint = 0.002;
+  double mu = 0.7;
+
+  // Set up the system
+  DiagramBuilder<double> builder;
+  auto [plant, scene_graph] = AddMultibodyPlant(config, &builder);
+
   std::cout << "Hello World!" << std::endl;
   return 0;
 }
