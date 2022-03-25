fix bugs to get acrobot working w/ custom gradients

acrobot.py
@@ -11,6 +11,11 @@ from pydrake.all import *
 from ilqr import IterativeLinearQuadraticRegulator
 import time
 
+# Choose what to do
+optimize = True
+playback = True
+debug_gradients = False
+
 ####################################
 # Parameters
 ####################################
@@ -25,6 +30,7 @@ MPC = False      # MPC only works with ilqr for now
 
 # Initial state
 x0 = np.array([0,0,0,0])
+#x0 = np.array([-0.22193181,  0.44017773, -5.15604732, 10.3126347 ])
 
 # Target state
 x_nom = np.array([np.pi,0,0,0])
@@ -48,6 +54,7 @@ def create_system_model(plant):
 ####################################
 # Create system diagram
 ####################################
+
 builder = DiagramBuilder()
 plant, scene_graph = AddMultibodyPlantSceneGraph(builder, dt)
 plant = create_system_model(plant)
@@ -59,7 +66,6 @@ builder.Connect(
         plant.get_actuation_input_port())
 
 DrakeVisualizer().AddToBuilder(builder, scene_graph)
-ConnectContactResultsToDrakeVisualizer(builder, plant, scene_graph)
 
 diagram = builder.Build()
 diagram_context = diagram.CreateDefaultContext()
@@ -70,136 +76,129 @@ plant_context = diagram.GetMutableSubsystemContext(
 # Solve Trajectory Optimization
 #####################################
 
-# System model for the trajectory optimizer
-builder_ = DiagramBuilder()
-plant_ = builder_.AddSystem(MultibodyPlant(dt))
-plant_ = create_system_model(plant_)
-builder_.ExportInput(plant_.get_actuation_input_port(), "control")
-system_ = builder_.Build()
+if optimize:
+    # System model for the trajectory optimizer
+    builder_ = DiagramBuilder()
+    plant_ = builder_.AddSystem(MultibodyPlant(dt))
+    plant_ = create_system_model(plant_)
+    builder_.ExportInput(plant_.get_actuation_input_port(), "control")
+    system_ = builder_.Build()
 
-#-----------------------------------------
-# DDP method
-#-----------------------------------------
+    #-----------------------------------------
+    # DDP method
+    #-----------------------------------------
 
-def solve_ilqr(solver, x0, u_guess):
-    """
-    Convienience function for solving the optimization
-    problem from the given initial state with the given
-    guess of control inputs.
-    """
-    solver.SetInitialState(x0)
-    solver.SetInitialGuess(u_guess)
+    def solve_ilqr(solver, x0, u_guess):
+        """
+        Convienience function for solving the optimization
+        problem from the given initial state with the given
+        guess of control inputs.
+        """
+        solver.SetInitialState(x0)
+        solver.SetInitialGuess(u_guess)
 
-    states, inputs, solve_time, optimal_cost = solver.Solve()
-    return states, inputs, solve_time, optimal_cost
+        states, inputs, solve_time, optimal_cost = solver.Solve()
+        return states, inputs, solve_time, optimal_cost
 
-if method == "ilqr":
-    # Set up optimizer
-    num_steps = int(T/dt)
-    ilqr = IterativeLinearQuadraticRegulator(system_, num_steps, 
-            beta=0.5, autodiff=False)
+    if method == "ilqr":
+        # Set up optimizer
+        num_steps = int(T/dt)
+        ilqr = IterativeLinearQuadraticRegulator(system_, num_steps, 
+                beta=0.5, autodiff=False)
 
-    # Define the problem
-    ilqr.SetTargetState(x_nom)
-    ilqr.SetRunningCost(dt*Q, dt*R)
-    ilqr.SetTerminalCost(Qf)
+        # Define the problem
+        ilqr.SetTargetState(x_nom)
+        ilqr.SetRunningCost(dt*Q, dt*R)
+        ilqr.SetTerminalCost(Qf)
 
-    # Set initial guess
-    u_guess = np.zeros((1,num_steps-1))
+        # Set initial guess
+        u_guess = np.zeros((1,num_steps-1))
 
+        if MPC:
+            # MPC parameters
+            num_resolves = 50    # total number of times to resolve the optimizaiton problem
+            replan_steps = 2     # number of timesteps after which to move the horizon and
+                                 # re-solve the MPC problem (>0)
+            total_num_steps = num_steps + replan_steps*num_resolves
+            total_T = total_num_steps*dt
 
-    if MPC:
-        # MPC parameters
-        num_resolves = 50    # total number of times to resolve the optimizaiton problem
-        replan_steps = 2     # number of timesteps after which to move the horizon and
-                             # re-solve the MPC problem (>0)
-        total_num_steps = num_steps + replan_steps*num_resolves
-        total_T = total_num_steps*dt
+            states = np.zeros((4,total_num_steps))
 
-        states = np.zeros((4,total_num_steps))
-
-        # Solve to get an initial trajectory
-        x, u, _, _ = solve_ilqr(ilqr, x0, u_guess)
-        states[:,0:num_steps] = x
-
-        for i in range(num_resolves):
-            # Set new state and control input
-            last_u = u[:,-1]
-            u_guess = np.block([
-                u[:,replan_steps:],    # keep same control inputs from last optimal sol'n
-                np.repeat(last_u[np.newaxis].T,replan_steps,axis=1)  # for new timesteps copy
-                ])                                                   # the last known control input
-            x0 = x[:,replan_steps]
-
-            # Resolve the optimization
+            # Solve to get an initial trajectory
             x, u, _, _ = solve_ilqr(ilqr, x0, u_guess)
+            states[:,0:num_steps] = x
 
-            # Save the result for playback
-            start_idx = (i+1)*replan_steps
-            end_idx = start_idx + num_steps
-            states[:,start_idx:end_idx] = x
+            for i in range(num_resolves):
+                # Set new state and control input
+                last_u = u[:,-1]
+                u_guess = np.block([
+                    u[:,replan_steps:],    # keep same control inputs from last optimal sol'n
+                    np.repeat(last_u[np.newaxis].T,replan_steps,axis=1)  # for new timesteps copy
+                    ])                                                   # the last known control input
+                x0 = x[:,replan_steps]
 
-        timesteps = np.arange(0.0,total_T,dt)
-    else:
-        states, inputs, solve_time, optimal_cost = solve_ilqr(ilqr, x0, u_guess)
-        print(f"Solved in {solve_time} seconds using iLQR")
+                # Resolve the optimization
+                x, u, _, _ = solve_ilqr(ilqr, x0, u_guess)
+
+                # Save the result for playback
+                start_idx = (i+1)*replan_steps
+                end_idx = start_idx + num_steps
+                states[:,start_idx:end_idx] = x
+
+            timesteps = np.arange(0.0,total_T,dt)
+        else:
+            states, inputs, solve_time, optimal_cost = solve_ilqr(ilqr, x0, u_guess)
+            print(f"Solved in {solve_time} seconds using iLQR")
+            print(f"Optimal cost: {optimal_cost}")
+            timesteps = np.arange(0.0,T,dt)
+
+    #-----------------------------------------
+    # Direct Transcription method
+    #-----------------------------------------
+
+    elif method == "sqp":
+        context_ = plant_.CreateDefaultContext()
+        input_port_index = plant_.get_actuation_input_port().get_index()
+
+        # Set up the solver object
+        trajopt = DirectTranscription(
+                plant_, context_, 
+                input_port_index=input_port_index,
+                num_time_samples=int(T/dt))
+        
+        # Add constraints
+        x = trajopt.state()
+        u = trajopt.input()
+        x_init = trajopt.initial_state()
+        
+        trajopt.prog().AddConstraint(eq( x_init, x0 ))
+        x_err = x - x_nom
+        trajopt.AddRunningCost(x_err.T@Q@x_err + u.T@R@u)
+        trajopt.AddFinalCost(x_err.T@Qf@x_err)
+        
+        # Solve the optimization problem
+        st = time.time()
+        res = Solve(trajopt.prog())
+        solve_time = time.time() - st
+        assert res.is_success(), "trajectory optimizer failed"
+        solver_name = res.get_solver_id().name()
+        optimal_cost = res.get_optimal_cost()
+        print(f"Solved in {solve_time} seconds using SQP via {solver_name}")
         print(f"Optimal cost: {optimal_cost}")
-        timesteps = np.arange(0.0,T,dt)
+        
+        # Extract the solution
+        timesteps = trajopt.GetSampleTimes(res)
+        states = trajopt.GetStateSamples(res)
+        inputs = trajopt.GetInputSamples(res)
 
-#-----------------------------------------
-# Direct Transcription method
-#-----------------------------------------
-
-elif method == "sqp":
-    context_ = plant_.CreateDefaultContext()
-    input_port_index = plant_.get_actuation_input_port().get_index()
-
-    # Set up the solver object
-    trajopt = DirectTranscription(
-            plant_, context_, 
-            input_port_index=input_port_index,
-            num_time_samples=int(T/dt))
-    
-    # Add constraints
-    x = trajopt.state()
-    u = trajopt.input()
-    x_init = trajopt.initial_state()
-    
-    trajopt.prog().AddConstraint(eq( x_init, x0 ))
-    x_err = x - x_nom
-    trajopt.AddRunningCost(x_err.T@Q@x_err + u.T@R@u)
-    trajopt.AddFinalCost(x_err.T@Qf@x_err)
-    
-    # Solve the optimization problem
-    st = time.time()
-    res = Solve(trajopt.prog())
-    solve_time = time.time() - st
-    assert res.is_success(), "trajectory optimizer failed"
-    solver_name = res.get_solver_id().name()
-    optimal_cost = res.get_optimal_cost()
-    print(f"Solved in {solve_time} seconds using SQP via {solver_name}")
-    print(f"Optimal cost: {optimal_cost}")
-    
-    # Extract the solution
-    timesteps = trajopt.GetSampleTimes(res)
-    states = trajopt.GetStateSamples(res)
-    inputs = trajopt.GetInputSamples(res)
-
-else:
-    raise ValueError("Unknown method %s"%method)
+    else:
+        raise ValueError("Unknown method %s"%method)
 
 #####################################
 # Playback
 #####################################
 
-def playback(states, timesteps):
-    """
-    Convienience function for visualising the given trajectory.
-
-    Relies on diagram, diagram_context, plant, and plant_context
-    being defined outside of the scope of this function and connected
-    to the Drake visualizer.
-    """
+if playback:
     while True:
         # Just keep playing back the trajectory
         for i in range(len(timesteps)):
@@ -213,4 +212,84 @@ def playback(states, timesteps):
             time.sleep(dt-3e-4)
         time.sleep(1)
 
-playback(states,timesteps)
+###################################
+# Do some gradient debugging
+###################################
+
+if debug_gradients:
+    import matplotlib.pyplot as plt
+
+    u0 = np.zeros(plant.num_actuators())
+    nx = plant.num_multibody_states()
+    nq = plant.num_positions()
+    nv = plant.num_velocities()
+
+    # Get gradients via custom method
+    plant.get_actuation_input_port().FixValue(plant_context, u0)
+    plant.SetPositionsAndVelocities(plant_context, x0)
+    st = time.time()
+    x_next, fx, fu = plant.DiscreteDynamicsWithApproximateGradients(plant_context)
+    print("our time: ", time.time()-st)
+
+    # Get gradients via autodiff
+    diagram_ad = diagram.ToAutoDiffXd()
+    context_ad = diagram_ad.CreateDefaultContext()
+    plant_ad = diagram_ad.GetSubsystemByName("plant")
+    plant_context_ad = diagram_ad.GetMutableSubsystemContext(plant_ad, context_ad)
+    xu_ad = InitializeAutoDiff(np.hstack([x0,u0]))
+    x_ad = xu_ad[:plant.num_multibody_states()]
+    u_ad = xu_ad[plant.num_multibody_states():]
+    
+    plant_ad.get_actuation_input_port().FixValue(plant_context_ad, u_ad)
+    plant_ad.SetPositionsAndVelocities(plant_context_ad, x_ad)
+    state = context_ad.get_discrete_state()
+    st = time.time()
+    diagram_ad.CalcDiscreteVariableUpdates(context_ad, state)
+    print("autodiff time: ", time.time()-st)
+    x_next_ad = state.get_vector().CopyToVector()
+    G = ExtractGradient(x_next_ad)
+    fx_ad = G[:,:nx]
+    fu_ad = G[:,nx:]
+
+    # zoom in on a particular part
+    #fx = fx[16:,:16]
+    #fx_ad = fx_ad[16:,:16]
+
+    # Visualize the gradients
+    min_ = np.amin(np.hstack([fx,fx_ad]))
+    max_ = np.amax(np.hstack([fx,fx_ad]))
+
+    plt.subplot(2,2,1)
+    plt.imshow(fx, vmin=min_, vmax=max_)
+    plt.title("fx ours")
+    plt.subplot(2,2,2)
+    plt.imshow(fx_ad, vmin=min_, vmax=max_)
+    plt.title("fx autodiff")
+    plt.subplot(2,2,3)
+    plt.imshow(fu)
+    plt.title("fu ours")
+    plt.subplot(2,2,4)
+    plt.imshow(fu_ad)
+    plt.title("fu autodiff")
+
+    # Do some computations related to the error
+    err = fx-fx_ad
+    dq_dq_err = np.amax(np.abs(err[:nq,:nq]))
+    dv_dq_err = np.amax(np.abs(err[nq:,:nq]))
+    dv_dv_err = np.amax(np.abs(err[nq:,nq:]))
+    dq_dv_err = np.amax(np.abs(err[:nq,nq:]))
+
+    print("\nMax errors:")
+    print(f"    dq_dq {dq_dq_err}")
+    print(f"    dq_dv {dq_dv_err}")
+    print(f"    dv_dq {dv_dq_err}")
+    print(f"    dv_dv {dv_dv_err}")
+
+    plt.figure()
+    plt.imshow(err)
+    plt.title("fx error")
+
+    plt.imshow(fx_ad, vmin=min_, vmax=max_)
+
+    plt.show()
+

allegro_hand.py
@@ -13,15 +13,15 @@ import time
 
 # Choose what to do
 simulate = False
-optimize = False
-playback = False
-debug_gradients = True
+optimize = True 
+playback = True
+debug_gradients = False
 
 ####################################
 # Parameters
 ####################################
 
-T = 0.5               # total simulation time (S)
+T = 0.1               # total simulation time (S)
 dt = 1e-3             # simulation timestep
 playback_rate = 1.0   # simulation rate
 
@@ -34,8 +34,8 @@ x0 = np.hstack([q0,v0])
 x_nom = 0.5 + np.zeros(32)
 
 # Quadratic cost int_{0^T} (x'Qx + u'Ru) + x_T*Qf*x_T
-Qq_hand = 0.01*np.ones(16)
-Qv_hand = 0.0*np.ones(16)
+Qq_hand = 0.1*np.ones(16)
+Qv_hand = 0.01*np.ones(16)
 
 Q = np.diag(np.hstack([Qq_hand, Qv_hand]))
 R = np.eye(16)
@@ -61,6 +61,9 @@ def create_system_model(builder, dt):
                      X_hand)
 
     # Add something to manipulate
+   
+    # Turn off gravity
+    #plant.mutable_gravity_field().set_gravity_vector([0,0,0])
 
     plant.Finalize()
     plant.set_name("plant")
@@ -96,7 +99,7 @@ if optimize:
     # Set up the optimizer
     num_steps = int(T/dt)
     ilqr = IterativeLinearQuadraticRegulator(system_, num_steps, 
-            beta=0.9, autodiff=False)
+            beta=0.9, autodiff=True)
 
     # Define initial and target states
     ilqr.SetInitialState(x0)
@@ -170,7 +173,9 @@ if debug_gradients:
     # Get gradients via custom method
     plant.get_actuation_input_port().FixValue(plant_context, u0)
     plant.SetPositionsAndVelocities(plant_context, x0)
+    st = time.time()
     x_next, fx, fu = plant.DiscreteDynamicsWithApproximateGradients(plant_context)
+    print("our time: ", time.time()-st)
 
     # Get gradients via autodiff
     diagram_ad = diagram.ToAutoDiffXd()
@@ -184,18 +189,27 @@ if debug_gradients:
     plant_ad.get_actuation_input_port().FixValue(plant_context_ad, u_ad)
     plant_ad.SetPositionsAndVelocities(plant_context_ad, x_ad)
     state = context_ad.get_discrete_state()
+    st = time.time()
     diagram_ad.CalcDiscreteVariableUpdates(context_ad, state)
+    print("autodiff time: ", time.time()-st)
     x_next_ad = state.get_vector().CopyToVector()
     G = ExtractGradient(x_next_ad)
     fx_ad = G[:,:plant.num_multibody_states()]
     fu_ad = G[:,plant.num_multibody_states():]
 
+    # zoom in on a particular part
+    #fx = fx[16:,:16]
+    #fx_ad = fx_ad[16:,:16]
+
     # Visualize the gradients
+    min_ = np.amin(np.hstack([fx,fx_ad]))
+    max_ = np.amax(np.hstack([fx,fx_ad]))
+
     plt.subplot(2,2,1)
-    plt.imshow(fx)
+    plt.imshow(fx, vmin=min_, vmax=max_)
     plt.title("fx ours")
     plt.subplot(2,2,2)
-    plt.imshow(fx_ad)
+    plt.imshow(fx_ad, vmin=min_, vmax=max_)
     plt.title("fx autodiff")
     plt.subplot(2,2,3)
     plt.imshow(fu)
@@ -203,6 +217,23 @@ if debug_gradients:
     plt.subplot(2,2,4)
     plt.imshow(fu_ad)
     plt.title("fu autodiff")
+
+    # Do some computations related to the error
+    err = fx-fx_ad
+    dq_dq_err = np.amax(np.abs(err[:16,:16]))
+    dv_dq_err = np.amax(np.abs(err[16:,:16]))
+    dv_dv_err = np.amax(np.abs(err[16:,16:]))
+    dq_dv_err = np.amax(np.abs(err[:16,16:]))
+
+    print("\nMax errors:")
+    print(f"    dq_dq {dq_dq_err}")
+    print(f"    dq_dv {dq_dv_err}")
+    print(f"    dv_dq {dv_dq_err}")
+    print(f"    dv_dv {dv_dv_err}")
+
+    plt.figure()
+    plt.imshow(err)
+    plt.title("fx error")
 
     plt.show()
 

cart_pole.py
@@ -23,14 +23,14 @@ dt = 1e-2      # simulation timestep
 method = "ilqr"
 
 # Initial state
-x0 = np.array([0.0,np.pi/2,0,0])
+x0 = np.array([0.0,np.pi-0.2,0,0])
 
 # Target state
 x_nom = np.array([0,np.pi,0,0])
 
 # Quadratic cost int_{0^T} (x'Qx + u'Ru) + x_T*Qf*x_T
 Q = np.diag([10,10,0.1,0.1])
-R = 0.001*np.eye(1)
+R = 0.01*np.eye(1)
 Qf = np.diag([100,100,10,10])
 
 ####################################
@@ -84,7 +84,7 @@ if method == "ilqr":
     # Set up the optimizer
     num_steps = int(T/dt)
     ilqr = IterativeLinearQuadraticRegulator(system_, num_steps, 
-            beta=0.9, autodiff=False)
+            beta=0.9, autodiff=True)
 
     # Define initial and target states
     ilqr.SetInitialState(x0)

cart_pole_with_wall.py
@@ -20,19 +20,19 @@ dt = 1e-2
 playback_rate = 1.0
 
 # Initial state
-x0 = np.array([0.0,np.pi+0.5,0,0])
+x0 = np.array([0.0,np.pi+0.5,-0.5,0])
 
 # Target state
 x_nom = np.array([0,np.pi,0,0])
 
 # Quadratic cost
-Q = np.diag([0.1,1,0.01,0.01])
+Q = np.diag([0.1,0.1,0.1,0.1])
 R = 0.001*np.eye(1)
 Qf = np.diag([200,200,10,10])
 
 # Contact model parameters
-dissipation = 1.0              # controls "bounciness" of collisions: lower is bouncier
-hydroelastic_modulus = 2e5     # controls "squishiness" of collisions: lower is squishier
+dissipation = 0.0              # controls "bounciness" of collisions: lower is bouncier
+hydroelastic_modulus = 2e6     # controls "squishiness" of collisions: lower is squishier
 resolution_hint = 0.05         # smaller means a finer mesh
 penetration_allowance = 0.1    # controls "softenss" of collisions for point contact model
 mu = 0.5                       # friction coefficient
@@ -84,6 +84,9 @@ def create_system_model(plant):
         AddContactMaterial(dissipation=dissipation, friction=CoulombFriction(), properties=wall_props)
     plant.RegisterCollisionGeometry(wall, RigidTransform(), 
             Box(l,w,h), "wall_collision", wall_props)
+
+    # Turn off gravity
+    #plant.mutable_gravity_field().set_gravity_vector([0,0,0])
     
     # Choose contact model
     plant.set_contact_surface_representation(mesh_type)
@@ -161,7 +164,7 @@ print(fu_ad)
 
 # Set up the optimizer
 num_steps = int(T/dt)
-ilqr = IterativeLinearQuadraticRegulator(system_, num_steps, beta=0.5, autodiff=False)
+ilqr = IterativeLinearQuadraticRegulator(system_, num_steps, beta=0.5, autodiff=True)
 
 # Define the optimization problem
 ilqr.SetInitialState(x0)

ilqr.py
@@ -45,18 +45,17 @@ class IterativeLinearQuadraticRegulator():
         self.context = self.system.CreateDefaultContext()
         self.input_port = self.system.get_input_port(input_port_index)
 
-        if autodiff:
-            # Make an autodiff copy of the system for computing dynamics gradients
-            self.system_ad = system.ToAutoDiffXd()
-            self.context_ad = self.system_ad.CreateDefaultContext()
-            self.input_port_ad = self.system_ad.get_input_port(input_port_index)
-        else:
-            # Extract the MultibodyPlant model from the given system. 
-            # The system in this case must include a MultibodyPlant called "plant"
-            # which is attached to a corresponding scene graph (for geometry computations)
-            self.plant = self.system.GetSubsystemByName("plant")
-            self.plant_context = self.system.GetMutableSubsystemContext(self.plant, self.context)
-
+        # Make an autodiff copy of the system for computing dynamics gradients
+        self.system_ad = system.ToAutoDiffXd()
+        self.context_ad = self.system_ad.CreateDefaultContext()
+        self.input_port_ad = self.system_ad.get_input_port(input_port_index)
+        
+        # Extract the MultibodyPlant model from the given system. 
+        # The system in this case must include a MultibodyPlant called "plant"
+        # which is attached to a corresponding scene graph (for geometry computations)
+        self.plant = self.system.GetSubsystemByName("plant")
+        self.plant_context = self.system.GetMutableSubsystemContext(self.plant, self.context)
+           
         # Set some parameters
         self.N = num_timesteps
         self.delta = delta
@@ -224,6 +223,7 @@ class IterativeLinearQuadraticRegulator():
 
         return x_next
 
+
     def _calc_dynamics_partials(self, x, u):
         """
         Compute dynamics partials 
@@ -231,35 +231,46 @@ class IterativeLinearQuadraticRegulator():
             x_next = f(x,u)
             fx = partial f(x,u) / partial x
             fu = partial f(x,u) / partial u
-       
-        using automatic differentiation.
         """
         if self.autodiff:
-            # Create autodiff versions of x and u
-            xu = np.hstack([x,u])
-            xu_ad = InitializeAutoDiff(xu)
-            x_ad = xu_ad[:self.n]
-            u_ad = xu_ad[self.n:]
-
-            # Set input and state variables in our stored model accordingly
-            self.context_ad.SetDiscreteState(x_ad)
-            self.input_port_ad.FixValue(self.context_ad, u_ad)
-
-            # Compute the forward dynamics x_next = f(x,u)
-            state = self.context_ad.get_discrete_state()
-            self.system_ad.CalcDiscreteVariableUpdates(self.context_ad, state)
-            x_next = state.get_vector().CopyToVector()
-           
-            # Compute partial derivatives
-            G = ExtractGradient(x_next)
-            fx = G[:,:self.n]
-            fu = G[:,self.n:]
-
+            return self._calc_dynamics_partials_ad(x,u)
         else:
-            # Use our custom dynamics gradient approximation, which exploits
-            # the structure of the TAMSI time-stepper.
-            x_next, fx, fu = \
-                    self.plant.DiscreteDynamicsWithApproximateGradients(self.plant_context)
+            return self._calc_dynamics_partials_custom(x,u)
+    
+    def _calc_dynamics_partials_ad(self, x, u):
+        """
+        Compute dynamics partials using automatic differentiation.
+        """
+        # Create autodiff versions of x and u
+        xu = np.hstack([x,u])
+        xu_ad = InitializeAutoDiff(xu)
+        x_ad = xu_ad[:self.n]
+        u_ad = xu_ad[self.n:]
+
+        # Set input and state variables in our stored model accordingly
+        self.context_ad.SetDiscreteState(x_ad)
+        self.input_port_ad.FixValue(self.context_ad, u_ad)
+
+        # Compute the forward dynamics x_next = f(x,u)
+        state = self.context_ad.get_discrete_state()
+        self.system_ad.CalcDiscreteVariableUpdates(self.context_ad, state)
+        x_next = state.get_vector().CopyToVector()
+       
+        # Compute partial derivatives
+        G = ExtractGradient(x_next)
+        fx = G[:,:self.n]
+        fu = G[:,self.n:]
+        
+        return (fx, fu)
+
+    def _calc_dynamics_partials_custom(self, x, u):
+        """
+        Compute dynamics partials using some (faster) custom methods.
+        """
+        self.context.SetDiscreteState(x)
+        self.input_port.FixValue(self.context, u)
+        x_next, fx, fu = \
+                self.plant.DiscreteDynamicsWithApproximateGradients(self.plant_context)
 
         return (fx, fu)
 

mini_cheetah.py
@@ -15,14 +15,14 @@ from ilqr import IterativeLinearQuadraticRegulator
 # Parameters
 ####################################
 
-T = 0.5
+T = 0.2
 dt = 5e-3
 playback_rate = 1.0
 target_vel = 0.50   # m/s
 
 # MPC parameters
-num_resolves = 0   # total number of times to resolve the optimizaiton problem
-replan_steps = 10   # number of timesteps after which to move the horizon and
+num_resolves = 100   # total number of times to resolve the optimizaiton problem
+replan_steps = 4   # number of timesteps after which to move the horizon and
                     # re-solve the MPC problem (>0)
 
 # Some useful definitions
@@ -53,23 +53,23 @@ x_nom[4] += target_vel*T  # base x position
 x_nom[22] += target_vel  # base x velocity
 
 # Quadratic cost
-Q_theta = 1.0*np.ones(4)    # body orientation
+Q_theta = 2.0*np.ones(4)    # body orientation
 Qf_theta = 10.0*np.ones(4)
 
-Q_p = np.array([0,10,0])        # body position
-Qf_p = 10.0*np.ones(3)
+Q_p = 1.0*np.array([0,1,0])        # body position
+Qf_p = 5.0*np.ones(3)
 
 Q_q = 0.0*np.ones(12)       # joint angles
 Qf_q = 0.1*np.ones(12)
 
-Q_omega = 0.0*np.ones(3)    # body angular velocity
+Q_omega = 0.01*np.ones(3)    # body angular velocity
 Qf_omega = 1.0*np.ones(3)
 
-Q_pdot = 0.0*np.ones(3)     # body linear velocity
-Qf_pdot = 0.0*np.ones(3)
+Q_pdot = 0.01*np.ones(3)     # body linear velocity
+Qf_pdot = 1.0*np.ones(3)
 
-Q_qdot = 0.0*np.ones(12)    # joint velocities
-Qf_qdot = 0.0*np.ones(12)
+Q_qdot = 0.01*np.ones(12)    # joint velocities
+Qf_qdot = 0.01*np.ones(12)
 
 Q = np.diag(np.hstack([
     Q_theta, Q_p, Q_q, Q_omega, Q_pdot, Q_qdot ]))
@@ -81,7 +81,7 @@ R = 0.01*np.eye(12)
 contact_model = ContactModel.kHydroelastic  # Hydroelastic, Point, or HydroelasticWithFallback
 mesh_type = HydroelasticContactRepresentation.kPolygon  # Triangle or Polygon
 
-mu = 0.2
+mu = 0.5
 
 dissipation = 0
 hydroelastic_modulus = 5e6
@@ -168,7 +168,7 @@ def solve_ilqr(solver, x0, u_guess, move_target=False):
 # Set up the optimizer
 num_steps = int(T/dt)
 ilqr = IterativeLinearQuadraticRegulator(system_, num_steps, 
-        beta=0.5, delta=1e-2, gamma=0, autodiff=False)
+        beta=0.5, delta=1e-2, gamma=0, autodiff=True)
 
 # Define the optimization problem
 ilqr.SetTargetState(x_nom)